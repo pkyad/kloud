dd1d5bcfb5872026d070ed42f121c15e7203fa93
cioc-d3 <rajkumaraero93@gmail.com>
2021-02-06 19:20:19 +0530

    Abc

--- finance/static/js/app.sales.js
+++ finance/static/js/app.sales.js
@@ -233,24 +233,24 @@ $scope.getFilters()
 
 app.controller('businessManagement.finance.invoicing.explore', function($scope, $http, $aside, $state, Flash, $users, $filter,  $uibModal, $timeout) {
 
-  $scope.postPay = function(){
-
-    $http({
-      method: 'POST',
-      url: '/api/ERP/getPaymentLink/',
-      data : {
-        'id' : 'sale_18' ,
-       'successUrl':'http://localhost:8000',
-       'failureUrl':'http://localhost:8000',
-       'source': 'chatbot',
-       'uid': '1612342739382'
-      },
-    }).
-    then(function(response) {
-      window.location.href = response.data
-    })
-
-  }
+  // $scope.postPay = function(){
+  //
+  //   $http({
+  //     method: 'POST',
+  //     url: '/api/ERP/getPaymentLink/',
+  //     data : {
+  //       'id' : 'sale_18' ,
+  //      'successUrl':'http://localhost:8000',
+  //      'failureUrl':'http://localhost:8000',
+  //      'source': 'chatbot',
+  //      'uid': '1612342739382'
+  //     },
+  //   }).
+  //   then(function(response) {
+  //     window.location.href = response.data
+  //   })
+  //
+  // }
 
 
 $scope.min_height  = 70
