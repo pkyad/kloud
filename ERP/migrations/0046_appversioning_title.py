3a6a5d688b55ca367d4db9184f57313fbff6aa9b
deepthi <deepthi.avunuru@gmail.com>
2021-02-09 20:03:57 +0530

    deepthi

--- ERP/migrations/0046_appversioning_title.py
+++ ERP/migrations/0046_appversioning_title.py
@@ -12,9 +12,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='appversioning',
-            name='title',
-            field=models.CharField(blank=True, max_length=50, null=True),
-        ),
+        # migrations.AddField(
+        #     model_name='appversioning',
+        #     name='title',
+        #     field=models.CharField(blank=True, max_length=50, null=True),
+        # ),
     ]
